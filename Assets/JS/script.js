3bec2eab46e3a82894dc53d86d0e2d9566d3795d
juju2307 <metzjulie2@gmail.com>
2021-07-14 17:06:03 +0200

    Début du jeu pewpew

---
+++ Assets/JS/script.js
@@ -0,0 +1,10 @@
+let c = document.getElementById("PewPew");
+let ctx = c.getContext("2d");
+let canon1 = document.getElementById("canon");
+let boulet1 = document.getElementById("boulet");
+let target1 = document.getElementById("target");
+/*ctx.drawImage(canon1,10,10);
+ctx.drawImage(boulet1,10,10);
+ctx.drawImage(target1,10,10);*/
+
+
